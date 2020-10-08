58ce4deee92c47f4023b8afcf01975ea8eeac353
Kauk777 <curokky777@gmail.com>
2020-10-08 19:58:09 +0530

    [KAUSHAL]Refactored: combining methods by generics

--- src/main/java/com/bridgelabz/genericmax/MaxFinder.java
+++ src/main/java/com/bridgelabz/genericmax/MaxFinder.java
@@ -1,45 +1,30 @@
 package com.bridgelabz.genericmax;
 
-public class MaxFinder {
-	Integer x, y, z;
-	Float xF,yF,zF;
-	String xS,yS,zS;
+public class MaxFinder<T extends Comparable<T>> {
+	T x, y, z;
 
-	public Integer maximumIntValue(Integer x, Integer y, Integer z) {
-		Integer max = x;
+	public MaxFinder(T x, T y, T z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public T maximumValue() {
+		return this.maximumValue(x, y, z);
+	}
+
+	public <T extends Comparable<T>> T maximumValue(T x, T y, T z) {
+		T max = x;
 		if (y.compareTo(max) > 0)
 			max = y;
 		if (z.compareTo(max) > 0)
 			max = z;
-		printIntMax(x, y, z, max);
-		return max;
-	}
-	public Float maximumFloatValue(Float xF, Float yF, Float zF) {
-		Float max = xF;
-		if (yF.compareTo(max) > 0)
-			max = yF;
-		if (zF.compareTo(max) > 0)
-			max = zF;
-		printFloatMax(xF, yF, zF, max);
-		return max;
-	}
-	public String maximumStringValue(String xS, String yS, String zS) {
-		String max = xS;
-		if (yS.compareTo(max) > 0)
-			max = yS;
-		if (zS.compareTo(max) > 0)
-			max = zS;
-		printStringMax(xS, yS, zS, max);
+		printMax(x, y, z, max);
 		return max;
 	}
 
-	public void printIntMax(Integer x, Integer y, Integer z, Integer max) {
+	public <T> void printMax(T x, T y, T z, T max) {
 		System.out.println("Maximum of " + x + ", " + y + ", " + z + " is: " + max);
 	}
-	public void printFloatMax(Float xF, Float yF, Float zF, Float max) {
-		System.out.println("Maximum of " + xF + ", " + yF + ", " + zF + " is: " + max);
-	}
-	public void printStringMax(String xS, String yS, String zS,String max) {
-		System.out.println("Maximum of " + xS + ", " + yS + ", " + zS + " is: " + max);
-	}
+
 }
--- src/test/java/com/bridgelabz/genericmax/MaximumTest.java
+++ src/test/java/com/bridgelabz/genericmax/MaximumTest.java
@@ -1,68 +1,68 @@
 package com.bridgelabz.genericmax;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
 
 public class MaximumTest {
-	MaxFinder maxValue;
-
-	@Before
-	public void intalize() {
-		maxValue = new MaxFinder();
-	}
 
 	@Test
 	public void whenGivenIntegers_maximumAtFirst_shouldReturnTrue() {
-		Integer result = maxValue.maximumIntValue(7, 4, 3);
+
+		Integer result = new MaxFinder<Integer>(7, 4, 3).maximumValue();
 		Assert.assertSame(7, result);
 	}
 
 	@Test
 	public void whenGivenIntegers_maximumAtSecond_shouldReturnTrue() {
-		Integer result = maxValue.maximumIntValue(4, 7, 3);
+
+		Integer result = new MaxFinder<Integer>(4, 7, 3).maximumValue();
 		Assert.assertSame(7, result);
 	}
 
 	@Test
 	public void whenGivenIntegers_maximumAtThird_shouldReturnTrue() {
-		Integer result = maxValue.maximumIntValue(4, 3, 7);
+
+		Integer result = new MaxFinder<Integer>(4, 3, 7).maximumValue();
 		Assert.assertSame(7, result);
 	}
 
 	@Test
 	public void whenGivenFloat_maximumAtFirst_shouldReturnTrue() {
-		Float result = maxValue.maximumFloatValue(7.54f, 4.20f, 7.45f);
+
+		Float result = new MaxFinder<Float>(7.54f, 4.20f, 7.45f).maximumValue();
 		Assert.assertEquals(7.54f, result, 0.0f);
 	}
 
 	@Test
 	public void whenGivenFloat_maximumAtSecond_shouldReturnTrue() {
-		Float result = maxValue.maximumFloatValue(4.20f, 7.54f, 7.45f);
+
+		Float result = new MaxFinder<Float>(4.20f, 7.54f, 7.45f).maximumValue();
 		Assert.assertEquals(7.54f, result, 0.0f);
 	}
 
 	@Test
 	public void whenGivenFloat_maximumAtThird_shouldReturnTrue() {
-		Float result = maxValue.maximumFloatValue(4.20f, 7.45f, 7.54f);
+
+		Float result = new MaxFinder<Float>(7.45f, 4.20f, 7.54f).maximumValue();
 		Assert.assertEquals(7.54f, result, 0.0f);
 	}
 
 	@Test
 	public void whenGivenString_maximumAtFirst_shouldReturnTrue() {
-		String result = maxValue.maximumStringValue("Peach", "Apple", "Grape");
+		String result = new MaxFinder<String>("Peach", "Apple", "Grape").maximumValue();
 		Assert.assertEquals("Peach", result);
 	}
 
 	@Test
 	public void whenGivenString_maximumAtSecond_shouldReturnTrue() {
-		String result = maxValue.maximumStringValue("Apple", "Peach", "Grape");
+		String result = new MaxFinder<String>("Apple", "Peach", "Grape").maximumValue();
 		Assert.assertEquals("Peach", result);
 	}
 
 	@Test
 	public void whenGivenString_maximumAtThird_shouldReturnTrue() {
-		String result = maxValue.maximumStringValue("Apple", "Grape", "Peach");
+		String result = new MaxFinder<String>("Apple", "Grape", "Peach").maximumValue();
 		Assert.assertEquals("Peach", result);
 	}
+
 }
