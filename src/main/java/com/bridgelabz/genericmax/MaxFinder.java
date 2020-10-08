d6d22f68355f81f4de8824237695e86aaafff54b
Kauk777 <curokky777@gmail.com>
2020-10-08 23:36:55 +0530

    [KAUSHAL]Added: UC5 printng max element with multiple parameter

--- src/main/java/com/bridgelabz/genericmax/MaxFinder.java
+++ src/main/java/com/bridgelabz/genericmax/MaxFinder.java
@@ -7,13 +7,21 @@ import java.util.List;
 
 public class MaxFinder<T> {
 
+
 	public T maximumValue(T... elements) {
 		List<T> list=new ArrayList<>();
 		for(T e:elements)
 			list.add(e);
 		Collections.sort(list, Collections.reverseOrder());
-		System.out.println("Max of element is: "+list.get(0));
-		return list.get(0);
+		T max=list.get(0);
+		printMax(max, elements);
+		return max;
+	}
+	public <T> void printMax(T max, T... elements) {
+		System.out.print("The max element of ");
+		for(T e:elements)
+			System.out.print(e+", ");
+		System.out.print(" is: "+max+"\n");
 	}
 
 }
